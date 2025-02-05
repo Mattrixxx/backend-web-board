6004027c58d8bfb4e0795e9b9483362fe18a4b13
Ming(MM) <mingth.forwork@gmail.com>
2025-02-05 20:13:06 +0700

    add mock data

---
+++ src/mock.data.ts
@@ -0,0 +1,26 @@
+export const mockPosts = [
+  {
+    id: 1,
+    username: 'User1',
+    title: 'The Beginning of the End of the World',
+    content: 'The afterlife sitcom The Good Place...',
+    communityId: 1,
+    comments: [
+      { id: 1, username: 'User2', content: 'Great post!' },
+      { id: 2, username: 'User3', content: 'Interesting insights!' },
+    ],
+  },
+];
+
+export const mockCommunities = [
+  { id: 1, name: 'History' },
+  { id: 2, name: 'Food' },
+  { id: 3, name: 'Pets' },
+  { id: 4, name: 'Health' },
+  { id: 5, name: 'Fashion' },
+  { id: 6, name: 'Exercise' },
+  { id: 7, name: 'Others' },
+];
+
+
+export const mockUsers = [{ id: 1, username: 'User1' }];
